c102af453087f84bf5fd25dd88db088f1c6eb16b
Joel Gombin <joel.gombin@gmail.com>
2019-08-03 04:15:04 +0200

    clarification attribut height

--- inst/rmarkdown/templates/xaringan/skeleton/macros.js
+++ inst/rmarkdown/templates/xaringan/skeleton/macros.js
@@ -2,7 +2,7 @@ remark.macros.largeur = function (percentage) {
   var url = this;
   return '<img src="' + url + '" style="width: ' + percentage + '" />';
 };
-remark.macros.hauteur = function (percentage) {
+remark.macros.hauteur = function (pixels) {
   var url = this;
-  return '<img src="' + url + '" style="height: ' + percentage + '" />';
+  return '<img src="' + url + '" style="height: ' + pixels + '" />';
 };
